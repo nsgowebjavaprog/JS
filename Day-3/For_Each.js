6efb0cfc8e27d5a02ccabc96459ae279c9bb98c1
NS258 <154657596+nsgowebjavaprog@users.noreply.github.com>
2024-12-10 15:23:49 +0530

    Add files via upload
    ad

---
+++ Day-3/For_Each.js
@@ -0,0 +1,43 @@
+/*
+const num = [1,2,3,4,5,6,7,8,9,10]
+
+num.forEach( function (num){
+    console.log(num)
+})
+*/
+
+/*
+const num = [1,2,3,4,5,6,7,8,9,10]
+
+num.forEach((val)=>{
+    console.log(val)
+})
+
+/*
+
+const num = [5,6,7,8,9,10]
+
+num.forEach( (value, index) => {
+    console.log(index, value)
+} )
+
+*/
+
+const my_obj = [
+    {
+        as: 'asdasas',
+        gf: 'aewerghf'
+    },
+    {
+        as: 'asdasas',
+        gf: 'aewerghf'
+    },
+    {
+        as: 'asdasas',
+        gf: 'aewerghf'
+    }
+
+]
+my_obj.forEach((val) => {
+    console.log(val)
+})
---
+++ Day-3/How_Work.js
@@ -0,0 +1,15 @@
+let val1 = 10
+let val2 = 5
+
+function add(num1, num2){
+    let total = num1 + num2
+    return total
+}
+
+let res1 = add(val1, val2)  // 15
+let res2 = add(10,20)    // 30
+console.log(res1)
+console.log(res2)
+
+15
+30
